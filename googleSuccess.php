3f5ef72cdc96f7ebe4e4396e88853a71a37eac84
BuildTools <cwmulcah@mtu.edu>
2018-12-06 19:03:52 -0500

    Added query to check if valid student upon login

--- googleSuccess.php
+++ googleSuccess.php
@@ -2,8 +2,32 @@
 
 session_start();
 
-if (isset($_POST["loggedin"]))
-  echo "Success!";
+if (isset($_POST["email"]))
+{
+  $config = parse_ini_file("finaldb.ini");
+  $dbh = new PDO($config['dsn'], $config['username'], $config['password']);
+
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  try {
+    $statement = $dbh->prepare("SELECT email from student_fp where Email=:email");
+
+    $statement->execute(array(':email' => $_POST['email']));
+    $result = $statement->fetch();
+
+    if($result == false)
+    {
+      echo "Do you even go here?";
+      die();
+    }
+
+    echo "Success!";
+
+  } catch (PDOException $e) {
+    print "Error!" . $e -> getMessage()."<br/>";
+    die();
+  }
+}
 else
   echo "You shouldn't be here";
 ?>
