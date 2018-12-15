674603941b6627adaceb6e72642266c5c2c43bf2
Glen Halonen <glenhalonen@gmail.com>
2018-12-15 15:30:05 -0500

    View results page created

--- viewResults.php
+++ viewResults.php
@@ -1,15 +1,35 @@
 <?php
-
 session_start();
 
-var_dump($_SESSION);
+$resultTable = [];
+try{
+        $config = parse_ini_file("finaldb.ini");
+        $dbh = new PDO($config['dsn'], $config['username'], $config['password']);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        $statement = $dbh->prepare("SELECT * FROM result_fp");
+        $statement->execute();
+        $resultTable = $statement->fetchAll();
+        print_r($resultTable);
+} catch (PDOException $e) {
+        print "Error!".$e->getMessage()."<br/>";
+        die();
+}
+
+//create some exam buttons with exam total 
+foreach($resultTable
+        //call function
+
+function display() {
+//display results herer 
+//
+}
 ?>
 
 <html>
+        <body>
+                <h1>EXAM SCORES!!</h1>
 
-  <body>
-    <link rel="stylesheet" type="text/css" href="main.css">
-  </body>
+        </body>
 
-<html>
+</html>
