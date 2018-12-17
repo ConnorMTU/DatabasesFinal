bbb00d5e766e025b42afcd10c991223970e1afdc
BuildTools <cwmulcah@mtu.edu>
2018-12-16 23:56:56 -0500

    Populate questions from database and submit data through post form to next page. Todo: Write correct data to database

--- takeExam.php
+++ takeExam.php
@@ -8,27 +8,99 @@
 
 session_start();
 
-if (isset($_POST["exam"]))
+if (isset($_SESSION['taken']) && isset($_SESSION['exam']))
 {
   var_dump($_POST);
-  // If they've chosen an exam
-  // can use unset() to unset variable
-}
 
-else
+  echo "<br><br>";
+
+  echo "You just submitted an exam, please continue to the View Results page to see your results.";
+  unset($_SESSION['taken']);
+  unset($_SESSION['exam']);
+}
+else if (isset($_POST["exam"]))
 {
-  // User is picking their first exam
+  // If they've chosen an exam
+  // We don't want to use session here because we only want them taking it right
+  // after they choose an exam
+
+  // Start grabbing data for specific exam
   $config = parse_ini_file("finaldb.ini");
   $dbh = new PDO($config['dsn'], $config['username'], $config['password']);
 
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   try {
-    $statement = $dbh->prepare("SELECT * from exam_fp");
+    $statement = $dbh->prepare("SELECT question_number, points, question_prompt, choice_prompt, choices from questions_fp where name=:name");
+
+    $statement->execute(array('name' => $_POST['exam']));
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($result);
+
+    echo "<br><br>";
+
+    // Start building questions
+    //echo $result[0]['question_prompt'] . " (" . $result[0]['points'] . " pts)";
+
+    echo "<form action='takeExam.php' method='post'>";
+
+    // Iterate through multilevel array to start displaying all queried questions
+    for ($i = 0; $i < sizeof($result); $i++) {
+      echo $result[$i]['question_number'] . ". " . $result[$i]['question_prompt'] . " (" . $result[$i]['points'] . " pts)";
+      echo "<br>";
+
+      // For some reason data isn't stored correctly in table
+      if (strlen($result[$i]['choices']) == 0)
+      {
+        echo "Broken question... contact test administrator";
+        continue;
+      }
+
+      // Break apart choice_prompt
+      $choice_prompt = explode(",", $result[$i]['choice_prompt']);
+      $choices = explode(",", $result[$i]['choices']);
+
+      // Iterate through total number of choices and display
+      for ($j = 0; $j < sizeof($choices); $j++) {
+        echo "<input type='radio' name='" . $result[$i]['question_number'] . "' value='" . $choices[$j] . "'>" . $choice_prompt[$j];
+        echo "<br>";
+      }
+
+      echo "<br>";
+    }
+
+    echo "<br>";
+
+    $_SESSION['taken'] = true;
+    $_SESSION['exam'] = $_POST['exam'];
+
+    ?>
+
+    <!-- Placeholder button for submission, need to define submitExam() func -->
+    <input type="submit" value="Submit">
+  </form>
+    <?php
+
+  } catch (PDOException $e) {
+    print "Error!" . $e -> getMessage()."<br/>";
+    die();
+  }
+} else {
+  // User is picking their first exam
+  // Grab name data for all exams and use to display to user.
+  $config = parse_ini_file("finaldb.ini");
+  $dbh = new PDO($config['dsn'], $config['username'], $config['password']);
+
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  try {
+    $statement = $dbh->prepare("SELECT name from exam_fp");
 
     $statement->execute();
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+    // Start building drop down menu from available exams in DB
     echo "<div id='wrapper'>";
     echo "<select id='exam' name='Exam'>";
     echo "<option value=''></option>";
@@ -42,13 +114,17 @@ else
     <div id="hidden_form_container" style="display:none;"></div>
 
 <?php
-  } catch (PDOException $e) {}
+  } catch (PDOException $e) {
+    print "Error!" . $e -> getMessage()."<br/>";
+    die();
+  }
 }
-
 
 ?>
 <script>
 
+  // Resuse the function we used for OAuth to submit post data because we already
+  // had it so why do it differently if it works.
   function submitData() {
     var theForm, exam;
     theForm = document.createElement('form');
@@ -74,4 +150,4 @@ else
 
   </body>
 
-<html>
+</html>
